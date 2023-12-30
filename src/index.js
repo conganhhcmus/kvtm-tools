3a13fc5ee8dcd0893d2d0f841136ee38a09ad94a
conganhhcmus <conganhhcmus@gmail.com>
2023-12-31 03:47:51 +0700

    refactor code

--- src/index.js
+++ src/index.js
@@ -1,49 +1,52 @@
-const moment = require('moment');
-const forever = require('forever-monitor');
-const util = require('util');
-const exec = util.promisify(require('child_process').exec);
+const moment = require('moment')
+const forever = require('forever-monitor')
+const util = require('util')
+const exec = util.promisify(require('child_process').exec)
 
-const { AutoOptions } = require('./constants');
+const { AutoOptions } = require('./constants')
 
-const frequency = 100;
-const excludeDevices = ['emulator-5554'];
+const frequency = 100
+const excludeDevices = [] //['emulator-5556']
 const gameOptions = {
     hasEventTrees: true,
-    resetAfterLoops: 2,
+    resetAfterLoops: 1,
     hasOpenGame: true,
     runAuto: AutoOptions.ProduceAndSellItems,
 }
-let runningDevices = [];
+let runningDevices = []
 
-const child = new (forever.Monitor)('auto.js', {
+const child = new forever.Monitor('auto.js', {
     max: frequency,
-    silent: false,
+    silent: true,
     sourceDir: 'src',
-    args: [JSON.stringify(gameOptions), JSON.stringify(excludeDevices)]
-});
+    outFile: `logs/out.txt`,
+    errFile: `logs/err.txt`,
+    args: [JSON.stringify(gameOptions), JSON.stringify(excludeDevices)],
+})
 
 child.on('start', async function () {
-    let result = await exec('adb devices | grep emulator | cut -f1');
+    let result = await exec('adb devices | grep emulator | cut -f1')
+    let listDevices = result.stdout.trim('\n').split('\n')
+    runningDevices = listDevices.filter((device) => !excludeDevices.includes(device))
 
-    runningDevices = result.stdout.trim('\n').split('\n').filter(device => !excludeDevices.includes(device));
-    console.log(`Running on [${runningDevices}]`);
-    console.error('Start at ' + moment().format('LTS'));
-});
+    console.log(`Running on [${runningDevices}]`)
+    console.log('Start at ' + moment().format('LTS'))
+})
 
 child.on('restart', function () {
-    console.error('Restart ' + child.times + ' times at ' + moment().format('LTS'));
-});
+    console.log('Restart ' + child.times + ' times at ' + moment().format('LTS'))
+})
 
 child.on('exit', function () {
-    console.log('Exits!!!');
-});
+    console.log('Exits!!!')
+})
 
-child.start();
+child.start()
 
 // stop auto when Ctrl + C
 process.on('SIGINT', function () {
-    console.log(`\nStop Devices: [${runningDevices}]`);
-    runningDevices.forEach(device => exec(`adb -s ${device} shell kill $(adb -s ${device} shell pgrep monkey)`));
+    console.log(`\nStop Devices: [${runningDevices}]`)
+    runningDevices.forEach((device) => exec(`adb -s ${device} shell kill $(adb -s ${device} shell pgrep monkey)`))
 
-    process.exit();
+    process.exit()
 })
